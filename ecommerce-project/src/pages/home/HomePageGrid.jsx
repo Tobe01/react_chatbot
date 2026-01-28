f7f3841e1b599aec10fb91f28ec01052e6fa9898
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-01-27 17:13:54 -0800

    updates!

--- ecommerce-project/src/pages/home/HomePageGrid.jsx
+++ ecommerce-project/src/pages/home/HomePageGrid.jsx
@@ -1,80 +1,12 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { formatMoney } from "../../../utils/money";
-import checkMark from '../../assets/images/icons/checkmark.png';
+import { Products } from './products';
 
 export function HomePageGrid({products, getAppData}){
-  
-  const [ quantity, setQuantity ] = useState(1);
-
-  function selectQuantity(event){
-    const selectedItems = Number(event.target.value)
-    setQuantity(selectedItems)
-  }
-  
   return(
     <div className="products-grid">
         {products.map((product)=>{
-          
-          async function updateCart(){
-            await axios.post('/api/cart-items', {
-              productId: product.id,
-              quantity: quantity
-            });
-
-            await getAppData();
-          };
-
           return(
             <>
-              <div key={product.id} className="product-container">
-                    <div className="product-image-container">
-                      <img className="product-image"
-                        src={product.image} />
-                    </div>
-
-                    <div className="product-name limit-text-to-2-lines">
-                    {product.name}
-                    </div>
-
-                    <div className="product-rating-container">
-                      <img className="product-rating-stars"
-                        src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
-                      <div className="product-rating-count link-primary">
-                        {product.rating.count}
-                      </div>
-                    </div>
-
-                    <div className="product-price">
-                      {formatMoney(product.priceCents)}
-                    </div>
-
-                    <div className="product-quantity-container" value={quantity} onClick={selectQuantity}>
-                      <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                      </select>
-                    </div>
-
-                    <div className="product-spacer"></div>
-
-                    <div className="added-to-cart">
-                      <img src={checkMark} />
-                      Added
-                    </div>
-
-                    <button className="add-to-cart-button button-primary" onClick={updateCart}>
-                      Add to Cart
-                    </button>
-              </div>
+              <Products key={product.id} product={product} getAppData={getAppData} />
             </>
           )
           })
---
+++ ecommerce-project/src/pages/home/products.jsx
@@ -0,0 +1,86 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { formatMoney } from "../../../utils/money";
+import checkMark from '../../assets/images/icons/checkmark.png'
+
+export function Products({product, getAppData}) {
+  const [ quantity, setQuantity ] = useState(1);
+  const [ added, setAdded ] = useState(false);
+
+
+  function selectQuantity(event){
+    const selectedItems = Number(event.target.value)
+    setQuantity(selectedItems)
+  }
+
+  function displayAdded(){
+    setAdded(true);
+    setTimeout(()=>{
+      setAdded(false);
+    }, 3000)
+  }
+
+  async function updateCart() {
+    await axios.post("/api/cart-items", {
+      productId: product.id,
+      quantity: quantity,
+    });
+
+    await getAppData();
+    displayAdded();
+  }
+
+  return (
+    <div className="product-container">
+      <div className="product-image-container">
+        <img className="product-image" src={product.image} />
+      </div>
+
+      <div className="product-name limit-text-to-2-lines">{product.name}</div>
+
+      <div className="product-rating-container">
+        <img
+          className="product-rating-stars"
+          src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+        />
+        <div className="product-rating-count link-primary">
+          {product.rating.count}
+        </div>
+      </div>
+
+      <div className="product-price">{formatMoney(product.priceCents)}</div>
+
+      <div
+        className="product-quantity-container"
+        value={quantity}
+        onClick={selectQuantity}
+      >
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div className="product-spacer"></div>
+
+      {added && (
+        <div className="added-to-cart active">
+          <img src={checkMark} />
+          Added
+        </div>
+      )}
+
+      <button className="add-to-cart-button button-primary" onClick={updateCart} >
+        Add to Cart
+      </button>
+    </div>
+  );
+}
